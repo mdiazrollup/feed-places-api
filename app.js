ad498c5458aefd5844f5b3d3134a9d089500398a
Maria Diaz <mariangeles112@gmail.com>
2021-04-10 13:47:24 -0400

    New port settings

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(uri)
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((err) => {
     console.log(err);
