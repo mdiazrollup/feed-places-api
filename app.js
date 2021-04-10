9c0c49d9a1e6521fbd0c810a5d5a297f42fdbb3a
Maria Diaz <mariangeles112@gmail.com>
2021-04-10 14:13:44 -0400

    error code fix

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500);
+  res.status(typeof error.code === 'number' ? error.code : 500);
   res.json({ message: error.message || 'An unknown error ocurred!' });
 });
 
